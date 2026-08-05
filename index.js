a97fb9d7712f867587f101b1f544a8937bf24bbf
AlphaXIV012 <167891996+AlphaXIV012@users.noreply.github.com>
2026-08-05 03:30:13 -0400

    Stage 4: full CRUD

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 3010;
 
 app.use(express.json());
 
 const myTasks = [
-  {id: 1, title: "flyrank Tutorial", status: true, },
-  {id: 2, title: "flyrank assignment", status: false, },
-  {id: 3, title: "flyrank capstone", status: false, }
+  {id: 1, title: "flyrank Tutorial", done: true, },
+  {id: 2, title: "flyrank assignment", done: false, },
+  {id: 3, title: "flyrank capstone", done: false, }
 ];  
 
 let Tasks = myTasks.map((task) => ({...task}));
@@ -22,13 +23,53 @@ app.get("/tasks/:id", (req, res) => {
     const taskId = parseInt(req.params.id);
     const task = Tasks.find((task) => task.id === taskId);
     if (!task) {
-        return res.status(404).json({ error: "Task not found" });
+        return res.status(404).json({ error: `Task ${taskId} not found` });
     }
     res.json(task);
 });
 
+// Check the health of the server
 app.get("/health", (req, res) => {
     res.json({ status: "OK" });
+});
+
+// Create a new task
+app.post("/tasks", (req, res) => {
+    const { title, done } = req.body;
+    const newTask = { id: Tasks.length + 1, title, done };
+
+    if(!title) {
+        return res.status(400).json({ error: "Title is required" });
+    }
+    Tasks.push(newTask);
+    res.status(201).json(newTask);
+});
+
+// Update an existing task
+app.put("/tasks/:id", (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const { title, done } = req.body;
+    const task = Tasks.find((t) => t.id === taskId);
+
+    if (!task || !done) {
+        return res.status(404).json({ error: `Task ${taskId} not found` });
+    }
+
+    Object.assign(task, req.body)
+    res.status(201).json(task);
+});
+
+// Delete a task
+app.delete("/tasks/:id", (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const task = Tasks.findIndex((t) => t.id === taskId);
+
+    if (task === -1) {
+        return res.status(404).json({ error: `Task ${taskId} not found` });
+    }
+    Tasks.splice(task, 1);
+    res.status(200).json({ message: `Task ${taskId} deleted successfully` });
+
 });
 
 app.listen(port, () => {
